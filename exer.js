991a2a120366857c6a02b2e88543b25eceb19f7b
Edmond Akwasi <edmondakwasi133@gmail.com>
2023-03-06 03:02:09 +0000

    first commit

---
+++ exer.js
@@ -0,0 +1,25 @@
+const removeParagraph = document.body.children[2].children[4];
+
+function clickedButton() {
+    removeThirdParagraph.remove('p')
+}
+removeParagraph.addEventListener('click', clickedButton);
+
+const addBlueBackGroundColor = document.getElementById('Blue-background');
+
+function secondButtonClicked() {
+    backgroundColorForFirstParagraph.className = 'Back-ground-color'
+    
+}
+
+addBlueBackGroundColor.addEventListener('click', secondButtonClicked);
+
+
+
+const backgroundColorForFirstParagraph = document.body.children[2].children[1];
+
+const removeThirdParagraph = document.body.children[2].children[3];
+
+
+
+
